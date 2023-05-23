33aa0f4d5b60cd9f2dd5ae48303edbab5ad6a4f1
Victor Ajayi <ovajayi@gmail.com>
2023-05-23 07:40:26 +0100

    modify contactMe, projects, and footer

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,115 +1,113 @@
-import React from "react"; 
+import React from "react";
 import Iframe from "./Iframe";
 
 const ContactMe = () => {
   return (
     <>
-    <section id="contact">
-            <div className="container mx-auto px-4 md:px-8 flex flex-col md:flex-row">
-        <div className="md:w-1/2">
-          <div className="isolate bg-white px-6 sm:py-10 lg:px-8 w-100">
-            <div className="mx-auto max-w-2xl text-center">
-              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                Contact Me
-              </h2>
-              <p className="mt-2 text-lg leading-8 text-gray-600">
-                Need help with a project?. You can reach out to me here
-              </p>
-            </div>
-            <form
-              action="#"
-              method="POST"
-              className="mx-auto mt-16 max-w-xl sm:mt-8"
-            >
-              <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                <div className="sm:col-span-2">
-                  <label
-                    htmlFor="first-name"
-                    className="block text-sm font-semibold leading-6 text-gray-900"
-                  >
-                    Full Name
-                  </label>
-                  <div className="mt-1.5">
-                    <input
-                      type="text"
-                      name="first-name"
-                      id="first-name"
-                      autoComplete="given-name"
-                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus
+      <section id="contact">
+        <div className="container mx-auto px-4 md:px-8 flex flex-col md:flex-row">
+          <div className="md:w-1/2">
+            <div className="isolate bg-white px-6 sm:py-10 lg:px-8 w-100">
+              <div className="mx-auto max-w-2xl text-center">
+                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                  Contact Me
+                </h2>
+                <p className="mt-2 text-lg leading-8 text-gray-600">
+                  Need help with a project?. You can reach out to me here
+                </p>
+              </div>
+              <form
+                action="#"
+                method="POST"
+                className="mx-auto mt-16 max-w-xl sm:mt-8"
+              >
+                <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                  <div className="sm:col-span-2">
+                    <label
+                      htmlFor="first-name"
+                      className="block text-sm font-semibold leading-6 text-gray-900"
+                    >
+                      Full Name
+                    </label>
+                    <div className="mt-1.5">
+                      <input
+                        type="text"
+                        name="first-name"
+                        id="first-name"
+                        autoComplete="given-name"
+                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus
                       -inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
+                      />
+                    </div>
+                  </div>
+                  <div className="sm:col-span-2">
+                    <label
+                      htmlFor="company"
+                      className="block text-sm font-semibold leading-6 text-gray-900"
+                    >
+                      Company
+                    </label>
+                    <div className="mt-1.5">
+                      <input
+                        type="text"
+                        name="company"
+                        id="company"
+                        autoComplete="organization"
+                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus -inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+                  <div className="sm:col-span-2">
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-semibold leading-6 text-gray-900"
+                    >
+                      Email
+                    </label>
+                    <div className="mt-1.5">
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        autoComplete="email"
+                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+                  <div className="sm:col-span-2">
+                    <label
+                      htmlFor="message"
+                      className="block text-sm font-semibold leading-6 text-gray-900"
+                    >
+                      Message
+                    </label>
+                    <div className="mt-1.5">
+                      <textarea
+                        name="message"
+                        id="message"
+                        rows={4}
+                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        defaultValue={""}
+                      />
+                    </div>
                   </div>
                 </div>
-                <div className="sm:col-span-2">
-                  <label
-                    htmlFor="company"
-                    className="block text-sm font-semibold leading-6 text-gray-900"
+                <div className="mt-10 pb-5">
+                  <button
+                    type="submit"
+                    className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
-                    Company
-                  </label>
-                  <div className="mt-1.5">
-                    <input
-                      type="text"
-                      name="company"
-                      id="company"
-                      autoComplete="organization"
-                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus
-                -inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
+                    Let&apos;s talk
+                  </button>
                 </div>
-                <div className="sm:col-span-2">
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-semibold leading-6 text-gray-900"
-                  >
-                    Email
-                  </label>
-                  <div className="mt-1.5">
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      autoComplete="email"
-                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-                <div className="sm:col-span-2">
-                  <label
-                    htmlFor="message"
-                    className="block text-sm font-semibold leading-6 text-gray-900"
-                  >
-                    Message
-                  </label>
-                  <div className="mt-1.5">
-                    <textarea
-                      name="message"
-                      id="message"
-                      rows={4}
-                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      defaultValue={""}
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="mt-10 pb-5">
-                <button
-                  type="submit"
-                  className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Let&apos;s talk
-                </button>
-              </div>
-            </form>
+              </form>
+            </div>
+          </div>
+          <div className="md:w-1/2 pb-4/3 md:ml-10 ">
+            <Iframe />
           </div>
         </div>
-        <div className="md:w-1/2 pb-4/3 md:ml-10 ">
-          <Iframe />
-        </div>
-      </div>
-    </section>
-
+      </section>
     </>
   );
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
+    const year = new Date().getFullYear()
   return (
-    <div className='container mt-20 mx-auto md:px-28 p-16 bg-blue-500'>Footer</div>
-  )
-}
+    <div className="container mt-20 mx-auto md:px-28 p-6 bg-blue-500">
+      <div className="flex justify-center mb-2">
+        <a href="https://github.com/oluwatoyinn">
+          <Image
+            src="/icons8-github.svg"
+            alt="Img Logo"
+            className="dark:invert"
+            width={50}
+            height={10}
+            priority
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/ajayioluwatoyin/">
+          <Image
+            src="/icons8-linkedin.svg"
+            alt="Img Logo"
+            className="dark:invert"
+            width={50}
+            height={10}
+            priority
+          />
+        </a>
 
-export default Footer
+        <Image
+          src="/icons8-whatsapp.svg"
+          alt="Img Logo"
+          className="dark:invert"
+          width={50}
+          height={10}
+          priority
+        />
+      </div>
+      <h2 className="text-center">Designed By Victor Ajayi @ {year} </h2>
+    </div>
+  );
+};
+
+export default Footer;
